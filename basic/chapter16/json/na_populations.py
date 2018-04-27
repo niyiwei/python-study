c7152306d5cd1d1b4813ce94778e81adce2aa14f
phd <panhaidon@qq.com>
2018-04-27 11:34:51 +0800

    删除

--- basic/chapter16/json/na_populations.py
+++
@@ -1,8 +0,0 @@
-import pygal.maps.world
-
-wm = pygal.maps.world.World()
-wm.title = "Oeoykatuibs if Countries in North America"
-wm.add("North Americs", {'ca': 34126000, 'us': 309349000, 'mx': 113423000})
-wm.add("Asia", {'cn': 1312345678})
-
-wm.render_to_file('na_populations.svg')
--- basic/chapter18/learning_log/learning_logs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from learning_logs.models import Topic
-
-admin.site.register(Topic)
-# Register your models here.
